aaeb852636ae37c7ab284c5c38d369e0e8c7dead
Totikas <toth.geri.0130@gmail.com>
2026-04-10 10:13:09 +0200

    javitott

---
+++ templates/pages/cimlap.tpl.php
@@ -0,0 +1,59 @@
+<h2>Üdvözöllek a pizzériánk oldalán!</h2>
+
+<div class='hasábok'>
+    <article class='balhasáb'>
+        <p>
+        A pizzáink friss alapanyagokból készülnek, átadja az igazi olasz életérzést,
+        melynek alapja sok generáció tapasztalata.
+    </p>
+
+    <p>
+        Büszkék vagyunk arra, hogy minden pizzánkat kézzel nyújtjuk, és fatüzelésű
+        kemencében sütjük, pont úgy, ahogy azt Nápolyban csinálják. A tésztánk 48 órán
+        át kel, hogy igazán ropogós és könnyű legyen ez az a különbség, amit az első
+        falattól érezni fog.
+    </p>
+
+    <p>
+        Még mindig nem keltettük fel az érdeklődésedet?
+    </p>
+
+    <p>
+        A pizzáink készítése során nemcsak a minőségre, hanem az élményre is nagy
+        hangsúlyt fektetünk. Hiszünk abban, hogy egy jó pizza nem csupán étel, hanem
+        egy pillanat, amelyet meg kell élni legyen szó családi vacsoráról, baráti
+        összejövetelről vagy egy csendes estéről otthon.
+    </p>
+
+    <p>
+        Minden hozzávalónkat gondosan válogatjuk össze, a paradicsomszósztól kezdve
+        egészen a friss bazsalikomig. A feltéteket bőségesen, de harmóniában helyezzük
+        el, hogy minden falatban érezhető legyen az olasz konyha szenvedélye és egyszerű
+        eleganciája. A kemencénk hője pedig gondoskodik arról, hogy a tészta kívül
+        ropogós, belül pedig puha és levegős maradjon.
+    </p>
+    </article>
+
+
+        <div class='videok'>
+        <aside>
+            <div class='yt-wrapper'>
+            <iframe src="https://www.youtube.com/embed/1hDNqHk6DKw" allowfullscreen loading="lazy"></iframe>
+            </div>
+        </aside>
+        <aside>
+            <div class='yt-wrapper'>
+            <iframe src="https://www.youtube.com/embed/jU1R4GXhNGU" allowfullscreen loading="lazy" ></iframe> 
+            </div>
+        </aside>
+    </div>
+</div>
+
+<div class='gterkep'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2166.0248410096633!2d19.66995459772868!3d46.914344972063574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1shu!2shu!4v1775679549011!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+
+
+
+ 
