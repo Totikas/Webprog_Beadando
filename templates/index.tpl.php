f9e6995b6a57e7f645524723345c52e216e72097
Totikas <toth.geri.0130@gmail.com>
2026-04-27 15:42:56 +0200

    Navbar-Hamburger
    Navbar reszponzív tervezése

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -3,38 +3,59 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
 	<meta charset="utf-8">
 	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
 	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
 	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
 </head>
 <body>
-	<header>
-		<img src="./images/<?=$fejlec['kepforras']?>" alt="<?=$fejlec['kepalt']?>">
-		<h1><?= $fejlec['cim'] ?></h1>
-		<?php if (isset($fejlec['motto'])) { ?><h2><?= $fejlec['motto'] ?></h2><?php } ?>
-		<?php if(isset($_SESSION['login'])) { ?>Bejlentkezve: <strong><?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></strong><?php } ?>
-         <div id="wrapper">
-    <aside id="nav">
-        <nav>
-            <ul>
-                <?php foreach ($oldalak as $url => $oldal) { ?>
-                    <?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
-                        <li<?= (($oldal == $keres) ? ' class="active"' : '') ?>>
-                            <a href="<?= ($url == '/') ? '.' : $url ?>">
-                                <?= $oldal['szoveg'] ?>
-                            </a>
-                        </li>
-                    <?php } ?>
-                <?php } ?>
-            </ul>
-        </nav>
-    </aside>
-	</header>
-   
-    <div id="content">
-        <?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
+   <nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href=".">Webprog Beadandó</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="mainMenu">
+        <ul class="navbar-nav mr-auto">
+
+            <?php foreach ($oldalak as $url => $oldal): ?>
+                <?php
+                    $megjelenhet = (!isset($_SESSION['login']) && $oldal['menun'][0])
+                                   || (isset($_SESSION['login']) && $oldal['menun'][1]);
+                ?>
+
+                <?php if ($megjelenhet): ?>
+                    <li class="nav-item <?= ($oldal == $keres) ? 'active' : '' ?>">
+                        <a class="nav-link" href="<?= ($url == '/') ? '.' : $url ?>">
+                            <?= $oldal['szoveg'] ?>
+                        </a>
+                    </li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+
+        </ul>
+
+        <?php if (isset($_SESSION['login'])): ?>
+            <span class="navbar-text text-white">
+                Bejelentkezve: <?= $_SESSION['csn'] . " " . $_SESSION['un'] ?> (<?= $_SESSION['login'] ?>)
+            </span>
+        <?php endif; ?>
     </div>
+</nav>
+
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-12">
+            <?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
+        </div>
+    </div>
+</div>
+
 </div>
     <footer>
         <?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
